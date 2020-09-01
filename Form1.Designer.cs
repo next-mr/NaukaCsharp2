6007d10d056551dcfb476c9671118ac2d6a5341c
Marcin Rudziński <marcin.rudzinski@next-it.pl>
2020-09-01 17:47:27 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace NaukaCsharp2
+{
+    partial class NaukaC2
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Koniec = new System.Windows.Forms.Button();
+            this.Lista = new System.Windows.Forms.TextBox();
+            this.WczytajLista = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DictionaryLista = new System.Windows.Forms.TextBox();
+            this.WczytajDictionary = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.QueueLista = new System.Windows.Forms.TextBox();
+            this.QueueList = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Koniec
+            // 
+            this.Koniec.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Koniec.Location = new System.Drawing.Point(986, 859);
+            this.Koniec.Name = "Koniec";
+            this.Koniec.Size = new System.Drawing.Size(100, 43);
+            this.Koniec.TabIndex = 0;
+            this.Koniec.Text = "Zamknij";
+            this.Koniec.UseVisualStyleBackColor = true;
+            this.Koniec.Click += new System.EventHandler(this.Koniec_Click);
+            // 
+            // Lista
+            // 
+            this.Lista.Location = new System.Drawing.Point(12, 104);
+            this.Lista.Multiline = true;
+            this.Lista.Name = "Lista";
+            this.Lista.Size = new System.Drawing.Size(269, 170);
+            this.Lista.TabIndex = 1;
+            // 
+            // WczytajLista
+            // 
+            this.WczytajLista.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.WczytajLista.Location = new System.Drawing.Point(181, 280);
+            this.WczytajLista.Name = "WczytajLista";
+            this.WczytajLista.Size = new System.Drawing.Size(100, 43);
+            this.WczytajLista.TabIndex = 0;
+            this.WczytajLista.Text = "Wczytaj";
+            this.WczytajLista.UseVisualStyleBackColor = true;
+            this.WczytajLista.Click += new System.EventHandler(this.List_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(269, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "List<nazwa> :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DictionaryLista
+            // 
+            this.DictionaryLista.Location = new System.Drawing.Point(12, 375);
+            this.DictionaryLista.Multiline = true;
+            this.DictionaryLista.Name = "DictionaryLista";
+            this.DictionaryLista.Size = new System.Drawing.Size(269, 170);
+            this.DictionaryLista.TabIndex = 1;
+            // 
+            // WczytajDictionary
+            // 
+            this.WczytajDictionary.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.WczytajDictionary.Location = new System.Drawing.Point(181, 551);
+            this.WczytajDictionary.Name = "WczytajDictionary";
+            this.WczytajDictionary.Size = new System.Drawing.Size(100, 43);
+            this.WczytajDictionary.TabIndex = 0;
+            this.WczytajDictionary.Text = "Wczytaj";
+            this.WczytajDictionary.UseVisualStyleBackColor = true;
+            this.WczytajDictionary.Click += new System.EventHandler(this.WczytajDictionary_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(12, 326);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(269, 46);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Dictionary<typy>";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1074, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Kolejki listy obiekty";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(12, 597);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 46);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Queue <typ>";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // QueueLista
+            // 
+            this.QueueLista.Location = new System.Drawing.Point(12, 646);
+            this.QueueLista.Multiline = true;
+            this.QueueLista.Name = "QueueLista";
+            this.QueueLista.Size = new System.Drawing.Size(269, 170);
+            this.QueueLista.TabIndex = 1;
+            // 
+            // QueueList
+            // 
+            this.QueueList.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.QueueList.Location = new System.Drawing.Point(181, 822);
+            this.QueueList.Name = "QueueList";
+            this.QueueList.Size = new System.Drawing.Size(100, 43);
+            this.QueueList.TabIndex = 0;
+            this.QueueList.Text = "Wczytaj";
+            this.QueueList.UseVisualStyleBackColor = true;
+            this.QueueList.Click += new System.EventHandler(this.WczytajQueue_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 822);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Kasuj(odczytaj)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OdczytajQueue_Click);
+            // 
+            // NaukaC2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1098, 914);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.QueueList);
+            this.Controls.Add(this.QueueLista);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.WczytajDictionary);
+            this.Controls.Add(this.DictionaryLista);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.WczytajLista);
+            this.Controls.Add(this.Lista);
+            this.Controls.Add(this.Koniec);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "NaukaC2";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Nauka C# 2";
+            this.Load += new System.EventHandler(this.NaukaC2_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.NaukaC2_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.NaukaC2_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.NaukaC2_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Koniec;
+        private System.Windows.Forms.TextBox Lista;
+        private System.Windows.Forms.Button WczytajLista;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox DictionaryLista;
+        private System.Windows.Forms.Button WczytajDictionary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox QueueLista;
+        private System.Windows.Forms.Button QueueList;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NaukaCsharp2
+{
+    public partial class NaukaC2 : Form
+    {
+        public NaukaC2()
+        {
+            InitializeComponent();
+        }
+
+        //przesuwanie okna po ekranie bez belki dla okna
+        //przesuwanie okna po ekranie bez belki dla okna
+        bool dragging = false;
+        int xOffset = 0;
+        int yOffset = 0;
+        private void NaukaC2_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            xOffset = Cursor.Position.X - Location.X;
+            yOffset = Cursor.Position.Y - Location.Y;
+        }
+        private void NaukaC2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging)
+            {
+                Location = new Point(Cursor.Position.X - xOffset, Cursor.Position.Y - yOffset);
+                Update();
+            }
+        }
+        private void NaukaC2_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+        private void NaukaC2_Load(object sender, EventArgs e)
+        {
+            //załaduj na start
+
+        }
+
+        private void List_Click(object sender, EventArgs e)
+        {
+            Lista.Clear();
+            List<Auto> lista1 = new List<Auto>();
+            lista1.Add(new Auto("BMW1", "1500 cm3"));
+            lista1.Add(new Auto("BMW2", "2000 cm3"));
+            lista1.Add(new Auto("BMW3", "3000 cm3"));
+            //metody ... remove, sort, reverse, clear i inne jak najbardziej mozzna stosować
+
+            foreach (var pozycja in lista1)
+            {
+                Lista.AppendText(pozycja.Nazwa + " ");
+                Lista.AppendText(pozycja.Pojemnosc + Environment.NewLine);
+            }
+
+            Lista.AppendText("RAZEM : " + lista1.Count.ToString());
+        }
+
+        private void Koniec_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        class Auto
+        {
+            public string Nazwa { get; set; }
+            public string Pojemnosc { get; set; }
+            public Auto(string marka, string silnik)
+            {
+                Nazwa = marka;
+                Pojemnosc = silnik;
+            }
+        }
+
+        private void WczytajDictionary_Click(object sender, EventArgs e)
+        {
+            Dictionary<int, string> slwonik1 = new Dictionary<int, string>();
+            slwonik1.Add(1, "Marcin");
+            slwonik1.Add(2, "Edek");
+            slwonik1.Add(3, "Bożena");
+            slwonik1.Add(4, "Maciek");
+            //int - jako ID - nie moze sie powtarzać i nie musi być int moze byc string i tak samo nie moze sie powtarzac...
+            //metody ... remove, sort, reverse, clear i inne jak najbardziej mozzna stosować
+            foreach (var pozycja in slwonik1)
+            {
+                DictionaryLista.AppendText(pozycja.Key + " ");
+                DictionaryLista.AppendText(pozycja.Value + Environment.NewLine);
+            }
+
+            DictionaryLista.AppendText("RAZEM : " + slwonik1.Count.ToString());
+        }
+
+        private void WczytajQueue_Click(object sender, EventArgs e)
+        {
+            KolejkaTworzenie();
+        }
+
+        private void OdczytajQueue_Click(object sender, EventArgs e)
+        {
+            //kolejka1.Dequeue();
+            //na teraz kasowanie pierwszego elementu jest niewykonalne - brak wiedzy.... :-(
+        }
+
+        private void KolejkaTworzenie ()
+        {
+            Queue<string> kolejka1 = new Queue<string>();
+            kolejka1.Enqueue("1");
+            kolejka1.Enqueue("2");
+            kolejka1.Enqueue("3");
+            kolejka1.Enqueue("4");
+            kolejka1.Enqueue("Ala ma kota");
+            kolejka1.Enqueue("Marcin - koniec kolejnki");
+            //typ int, string....
+            foreach (var pozycja in kolejka1)
+            {
+                QueueLista.AppendText(pozycja + ", ");
+            }
+        }
+    }
+}
